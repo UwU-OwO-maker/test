f2535c0db032154305203682cd9a7cd5ad92595f
UwU-OwO-maker <61390224+UwU-OwO-maker@users.noreply.github.com>
2022-10-07 18:45:23 -0400

    Update bypass.lua

--- bypass.lua
+++ bypass.lua
@@ -346,7 +346,7 @@ function execute()
 		["no"] = "n{{aieixzvzx:o}}",
 		["so"] = "s{{aieixzvzx:o}}",
 		["said"] = "sa{{aieixzvzx:id}}",
-		["can"] = "ca{{aieixzvzx:n}}"
+		["can"] = "ca{{aieixzvzx:n}}",
 		["we"] = "w{{aieixzvzx:e}}",
 
 		
