1ecf69a7c4e4570d40f9fff487a1936b017f20ec
UwU-OwO-maker <61390224+UwU-OwO-maker@users.noreply.github.com>
2022-12-22 16:59:51 -0500

    Update lock.lua

--- lock.lua
+++ lock.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("test"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UwU-OwO-maker/test/main/lock2.lua"))()
 DaHoodSettings.Prediction = 0.135
 Aiming.TargetPart = {"Head", "LeftHand", "RightHand", "LeftLowerArm", "RightLowerArm", "LeftUpperArm", "RightUpperArm", "LeftFoot", "LeftLowerLeg", "UpperTorso", "HumanoidRootPart", "LeftUpperLeg", "RightLowerLeg", "RightFoot", "LowerTorso"}
 Aiming.FOV = 17.4
