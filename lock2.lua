e559b7762b1d1ee679664137f72dfc02f2c26f89
UwU-OwO-maker <61390224+UwU-OwO-maker@users.noreply.github.com>
2022-12-22 17:04:29 -0500

    Update lock2.lua

--- lock2.lua
+++ lock2.lua
@@ -14,7 +14,7 @@ local DaHoodSettings = {
     SilentAim = true,
     AimLock = false,
     Prediction = 0.14,
-    AimLockKeybind = Enum.KeyCode.E
+    AimLockKeybind = Enum.KeyCode.Q
 }
 getgenv().DaHoodSettings = DaHoodSettings
 
